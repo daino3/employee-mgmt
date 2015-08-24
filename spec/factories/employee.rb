5b1f5b0b31043a4f6176487ba2d8ebd7eca6be68
daino3 <dainovu3@gmail.com>
2015-08-23 23:56:17 -0500

    added employee factory

--- spec/factories/employee.rb
+++ spec/factories/employee.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
   factory :employee do
     sequence(:first_name) { |n| "employee-first-#{n}" }
     sequence(:last_name) { |n| "employee-last-#{n}" }
+    hire_date { (3.years.ago.to_date..Date.today).to_a.sample }
   end
 end
